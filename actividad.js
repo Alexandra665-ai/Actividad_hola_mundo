83bd2d35edc7307aad80a15183f39ca1443ebe3c
Alexandra665-ai <alexandra.m.t.665@gmail.com>
2026-04-10 13:48:41 -0400

    implementación lógica JavaScript

---
+++ actividad.js
@@ -0,0 +1,7 @@
+alert("Alerta Hola Mundo ");
+
+document.getElementById("mensaje-documento").textContent = "Hola Mundo ";
+
+console.log(" Hola Mundo ");
+
+document.getElementById("mensaje-div").textContent = " DIV Hola Mundo ";
